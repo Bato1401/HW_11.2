23ba67aec9d6edd7d3166047c9e9a391149beb28
Bato1401 <lego.1041@gmail.com>
2023-03-15 21:34:18 +0300

    add in str_func.py function text.title()

--- str_func.py
+++ str_func.py
@@ -1,5 +1,16 @@
 def large_letters(text):
+    """Функция переводит все буквы слов
+    в введенной строке в верхний регистр"""
     return text.upper()
 
 
-print(large_letters('python'))
+print(large_letters('python very good languages'))
+
+
+def large_first_letters(text):
+    """Функция переводит первые буквы слов
+    в введенной строке в верхний регистр"""
+    return text.title()
+
+print(large_first_letters('python very good languages'))
+
